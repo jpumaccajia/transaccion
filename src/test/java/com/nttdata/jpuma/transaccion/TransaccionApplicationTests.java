16005c2d20df7cce2c13d762913e1f130ba62421
Javier Serapio Pumaccajia Tasilla <jpumacca@emeal.nttdata.com>
2023-07-11 10:12:00 -0500

    Proyecto Inicial

---
+++ src/test/java/com/nttdata/jpuma/transaccion/TransaccionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nttdata.jpuma.transaccion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransaccionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
